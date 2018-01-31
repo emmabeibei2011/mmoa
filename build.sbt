406053e658c30751721058b9a6c48e06264d1c15
shiyancao <shawncaojob@gmail.com>
2018-01-30 23:14:16 -0500

    Some new changes

--- build.sbt
+++ build.sbt
@@ -2,8 +2,8 @@ name := "mediamath-oa"
 
 version := "0.1"
 
-scalaVersion := "2.12.4"
+scalaVersion := "2.11.8"
 
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.1"
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.1"
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.2.1"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.2.1"
--- src/main/scala/com/mediamath/oa/Solution.scala
+++ src/main/scala/com/mediamath/oa/Solution.scala
@@ -1,6 +1,7 @@
 package com.mediamath.oa
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Encoders
 
 
 object Solution {
@@ -11,8 +12,14 @@ object Solution {
       .config("spark.some.config.option", "some-value")
       .getOrCreate()
 
-    val events = spark.read.csv("file:/Users/dishao/SRC/mediamath-oa/events.csv")
+    val eventsSchema = Encoders.product[Event].schema
+    val events = spark.read.schema(eventsSchema).csv("file:/Users/dishao/SRC/mediamath-oa/events.csv")
 
-    val imps = spark.read.csv("file:/Users/dishao/SRC/mediamath-oa/impressions.csv")
+    val impsSchema = Encoders.product[Impression].schema
+    val imps = spark.read.schema(impsSchema).csv("file:/Users/dishao/SRC/mediamath-oa/impressions.csv")
   }
 }
+
+case class Event(ts: Int, eventId: String, adId: String, userId: String, eventType: String)
+
+case class Impression(ts: Int, adId: String, creativeId: String, userId: String)
