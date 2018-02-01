da1a533d9b054a999a393a1b086060bc2096e5c3
shiyancao <shawncaojob@gmail.com>
2018-02-01 07:28:30 -0500

    some changes

--- src/main/scala/com/mediamath/oa/Solution.scala
+++ src/main/scala/com/mediamath/oa/Solution.scala
@@ -23,3 +23,28 @@ object Solution {
 case class Event(ts: Int, eventId: String, adId: String, userId: String, eventType: String)
 
 case class Impression(ts: Int, adId: String, creativeId: String, userId: String)
+
+
+/*
+val textFile = sc.textFile("file:/Users/dishao/SRC/mediamath-oa/events.csv")
+
+events.orderBy("userId", "adId", "eventType", "ts")
+
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
+
+
+val wspec = Window.partitionBy("userId", "adId", "eventType").orderBy("ts")
+
+val e2 = events.withColumn("prevTs", lag(events("ts"), 1).over(wspec)).show()
+
+e2.collect.foreach(println)
+
+val dedup = e2.filter("prevTs is null or ts - prevTs >= 60")
+
+//join impression table on
+
+
+ */
+
+
